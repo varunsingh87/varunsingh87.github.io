e5716a9aba4ccf456f9f20bce1cdb2b5b741eb85
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-09 15:56:11 -0400

    Adjusted indentation to 2 spaces
    From 4 spaces per indent to 2 spaces per indent for comment

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
-/* 
-	Create new link element
-	Tell browser it is a favicon (in the title tab to the left an image)
-	Give link to image as the kangaroo with an 'ico' extension
-*/
+	/* 
+		Create new link element
+		Tell browser it is a favicon (in the title tab to the left an image)
+		Give link to image as the kangaroo with an 'ico' extension
+	*/
   var link = top.document.createElement("link");
   link.type = "image/x-icon";
   link.rel = "shortcut icon";
