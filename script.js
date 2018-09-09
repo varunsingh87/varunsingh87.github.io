500fe966797264161049b0bb4feb874c5a51175d
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-09 19:51:52 -0400

    Kangaroo Png with accurate attributions

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 window.onload = function() {
-	/* 
-		Create new link element
-		Tell browser it is a favicon (in the title tab to the left an image)
-		Give link to image as the kangaroo with an 'ico' extension
-	*/
   var link = top.document.createElement("link");
   link.type = "image/x-icon";
   link.rel = "shortcut icon";
-  link.href = "./Australian_Kangaroo.ico";
+  link.href = "/wiki.ico";
   top.document.getElementsByTagName("head")[0].appendChild(link);
 }
