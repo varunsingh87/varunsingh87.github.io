943d436bf68553d571c876e1db262f2c8ecace29
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-20 20:14:49 -0400

    Debugging log statement

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-window.onload = function() {
-  var link = top.document.createElement("link");
-  link.type = "image/x-icon";
-  link.rel = "shortcut icon";
-  link.href = "./icon.ico";
-  top.document.getElementsByTagName("head")[0].appendChild(link);
+const link = document.createElement("link");
+link.type = "image/x-icon";
+link.rel = "shortcut icon";
+link.href = "./icon.ico";
+document.getElementsByTagName("head")[0].appendChild(link);
 
-  const stylesheetLink = top.document.createElement("link");
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  link.href = "style.css";
-  top.document.getElementsByTagName("head")[0].appendChild(link)
-}
+const stylesheetLink = top.document.createElement("link");
+link.type = "text/css";
+link.rel = "stylesheet";
+link.href = "style.css";
+top.document.getElementsByTagName("head")[0].appendChild(link);
+
+console.log("The style was linked and the favicon was added.");
 
 function goToHomePage() {
 	window.location = "https://knowledgeablekangaroo.github.io";
