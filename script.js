0a0a6fed50ac7580190a0737f3ea321a0f5b8c1e
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-22 16:03:25 -0400

    removed top
    Top was stopping object elements from being styled

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const link = top.document.createElement("link");
+const link = document.createElement("link");
 link.type = "image/x-icon";
 link.rel = "shortcut icon";
 link.href = "./icon.ico";
-top.document.querySelector("head").appendChild(link);
+document.querySelector("head").appendChild(link);
 
-const stylesheetLink = top.document.createElement("link");
+const stylesheetLink = document.createElement("link");
 stylesheetLink.type = "text/css";
 stylesheetLink.rel = "stylesheet";
 stylesheetLink.href = "./style.css";
-top.document.querySelector("head").appendChild(stylesheetLink);
+document.querySelector("head").appendChild(stylesheetLink);
 
 console.log("The style was linked and the favicon was added.");
 
