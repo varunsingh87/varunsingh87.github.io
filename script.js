595516241dc516792961d1eb14fcbb9b63081add
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-09-21 21:26:58 -0400

    Removed index reference (error)

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const stylesheetLink = top.document.createElement("link");
 link.type = "text/css";
 link.rel = "stylesheet";
 link.href = "./style.css";
-top.document.querySelector("head")[0].appendChild(link);
+top.document.querySelector("head").appendChild(link);
 
 console.log("The style was linked and the favicon was added.");
 
