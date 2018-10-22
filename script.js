2476386307c7464b6dbb36680fc4f920b9ae15b0
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2018-10-21 21:45:28 -0400

    Check if linked files are in folder
    * Use XHR to check if the link is creating a 404
      - If the link exists, keep the link how it is
      - If the link doesn't exists, precede the link with the corresponding folder
    
    * Inserted more comments
    * Use the modern (ES8) fetch

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const link = document.createElement("link");
+let url = "";
+
+url = "icon.ico"; // Set url to check if the location being linked to is created
+const link = document.createElement("link"); // Create link element
 link.type = "image/x-icon";
 link.rel = "shortcut icon";
-link.href = "./icon.ico";
+link.href = url;
+urlExists(url, function(exists) { // Request to check if url exists
+	  if (!exists) {
+	  	link.href = "images/" + url;
+	  }
+	}
+);
 document.querySelector("head").appendChild(link);
 
+url = "style.css"; // Set url to check if the location being linked to is created
 const stylesheetLink = document.createElement("link");
 stylesheetLink.type = "text/css";
 stylesheetLink.rel = "stylesheet";
-stylesheetLink.href = "./style.css";
+stylesheetLink.href = url;
+urlExists(url, function(exists) { // Request to check if url exists
+	  if (!exists) {
+	    stylesheetLink.href = "css/" + url;
+	  }
+	}
+);
 document.querySelector("head").appendChild(stylesheetLink);
 
 console.log("The style was linked and the favicon was added.");
@@ -19,3 +35,11 @@ function goToHomePage() {
 function goBack() {
 	window.history.back();
 }
+
+function urlExists(url, callback) {
+  fetch(url)
+  .then(function(status) {
+    callback(ok)
+  });
+}
+
